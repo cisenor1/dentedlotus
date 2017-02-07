8b39739c8f04274b74d1e54f8c3b179f6099280a
Derrick Plotsky <derrickplotsky@gmail.com>
2017-02-06 19:25:17 -0800

    Add server code
     - Add all server code
     - Modify tsconfig.json to do build in-place (webpack still works)
     - Combine app.ts for hapi and express for now

--- Client/app.js
+++ Client/app.js
@@ -1,35 +1,56 @@
-/**
- * Copyright 2016, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// [START app]
+// A lot of the code for this came from https://auth0.com/blog/2016/03/07/hapijs-authentication-secure-your-api-with-json-web-tokens/
+// Specifically mostly the code around the security bits.
+// To access secured routes, an Authorization header must be set. 
+// ex: Authorization: Bearer <TOKEN>
 'use strict';
-
-const path = require('path');
-const express = require('express');
-const app = express(); 
-const app2 = express();
+var port = 32187;
+var express = require('express');
+var app = express();
 app.use(express.static("dist"));
-app2.get('/', (req, res) => {
-   res.status(200).send("Test!");
-});
-
+var hapi_1 = require("hapi");
+var config_1 = require("./app/config");
+// Routes import
+var races_1 = require("./app/routes/races");
+var challenges_1 = require("./app/routes/challenges");
+var drivers_1 = require("./app/routes/drivers");
+var users_1 = require("./app/routes/users");
+var blogs_1 = require("./app/routes/blogs");
 // Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function () {
+    console.log("App listening on port " + PORT);
+    console.log('Press Ctrl+C to quit.');
 });
-app2.listen(32187);
-// [END app]
+var server = new hapi_1.Server();
+server.connection({ port: port });
+// Register the JWT middleware for hapi
+server.register(require('hapi-auth-jwt'), function (err) {
+    // We're giving the strategy both a name
+    // and scheme of 'jwt'
+    server.auth.strategy('jwt', 'jwt', {
+        key: config_1.key,
+        verifyOptions: { algorithms: ['HS256'] }
+    });
+    // Let's make the routes a bit more exlicit instead of implicit
+    server.route(races_1.raceRoutes);
+    server.route(challenges_1.challengesRoutes);
+    server.route(drivers_1.driverRoutes);
+    server.route(users_1.userRoutes);
+    server.route(blogs_1.blogRoutes);
+    // Look through the routes in
+    // all the subdirectories of API
+    // and create a new route for each
+    // glob.sync('./routes/*.js', {
+    //   root: __dirname
+    // }).forEach(file => {
+    //   const route = require(path.join(__dirname, file));
+    //   console.log(route);
+    //   server.route(route);
+    // });
+});
+server.start(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log('server running on port ' + port);
+});
---
+++ Client/app.ts
@@ -0,0 +1,69 @@
+﻿// A lot of the code for this came from https://auth0.com/blog/2016/03/07/hapijs-authentication-secure-your-api-with-json-web-tokens/
+// Specifically mostly the code around the security bits.
+
+// To access secured routes, an Authorization header must be set. 
+// ex: Authorization: Bearer <TOKEN>
+
+'use strict';
+const port = 32187;
+
+const express = require('express');
+const app = express(); 
+app.use(express.static("dist"));
+
+import { Server } from "hapi";
+import * as glob from "glob";
+import * as path from "path";
+import { key as secret } from "./app/config";
+
+// Routes import
+import { raceRoutes } from "./app/routes/races";
+import { challengesRoutes } from "./app/routes/challenges";
+import { driverRoutes } from "./app/routes/drivers";
+import { userRoutes } from "./app/routes/users";
+import { blogRoutes } from "./app/routes/blogs";
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+const server = new Server();
+
+server.connection({ port: port });
+
+// Register the JWT middleware for hapi
+server.register(require('hapi-auth-jwt'), (err) => {
+  // We're giving the strategy both a name
+  // and scheme of 'jwt'
+  server.auth.strategy('jwt', 'jwt', {
+    key: secret,
+    verifyOptions: { algorithms: ['HS256'] }
+  });
+
+  // Let's make the routes a bit more exlicit instead of implicit
+  server.route(raceRoutes);
+  server.route(challengesRoutes);
+  server.route(driverRoutes);
+  server.route(userRoutes);
+  server.route(blogRoutes);
+  // Look through the routes in
+  // all the subdirectories of API
+  // and create a new route for each
+  // glob.sync('./routes/*.js', {
+  //   root: __dirname
+  // }).forEach(file => {
+  //   const route = require(path.join(__dirname, file));
+  //   console.log(route);
+  //   server.route(route);
+  // });
+});
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('server running on port ' + port);
+});
